7f995715ca72a38948bd4b431fa1f8f3b3b5c59d
Samwheelie <sawheeler209@gmail.com>
2025-06-25 23:55:59 -0700

    Error fixes for closing window. Detects clicking x event now.

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        screen.fill("black")
-        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+    screen.fill("black")
+    pygame.display.flip()
 
 if __name__ == "__main__":
     main()
